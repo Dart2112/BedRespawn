737875849a8348d03ae62cac06994568ac288882
dart2112 <DNAblue2112@gmail.com>
2018-11-22 11:55:36 +1030

    Fixed location finding for the head of the bed

--- src/main/java/net/lapismc/bedrespawn/Bed.java
+++ src/main/java/net/lapismc/bedrespawn/Bed.java
@@ -2,7 +2,6 @@ package net.lapismc.bedrespawn;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.block.Block;
 
 import java.util.UUID;
 
@@ -15,10 +14,11 @@ class Bed {
     Bed(BedRespawn plugin, UUID ownerUUID, Location bedLocation) {
         this.plugin = plugin;
         this.ownerUUID = ownerUUID;
-        this.bedLocation = bedLocation;
         if (!plugin.isBed(bedLocation.getBlock())) {
             plugin.getLogger().info("No bed was found at " + bedLocation.toString() + ", As such " + Bukkit.getOfflinePlayer(ownerUUID).getName() + " will not have a bed spawn");
             Bukkit.getScheduler().runTaskLater(plugin, () -> plugin.beds.remove(this), 5);
+        } else {
+            this.bedLocation = getBedHeadLocation(bedLocation);
         }
     }
 
@@ -43,11 +43,11 @@ class Bed {
     }
 
     private Location getBedHeadLocation(Location bedLocation) {
-        org.bukkit.material.Bed b = (org.bukkit.material.Bed) bedLocation.getBlock();
-        if (b.isHeadOfBed()) {
-            return (((Block) b).getRelative(b.getFacing())).getLocation();
+        org.bukkit.block.data.type.Bed b = (org.bukkit.block.data.type.Bed) bedLocation.getBlock().getBlockData();
+        if (b.getPart().equals(org.bukkit.block.data.type.Bed.Part.HEAD)) {
+            return bedLocation;
         } else {
-            return (((Block) b).getRelative(b.getFacing().getOppositeFace())).getLocation();
+            return ((bedLocation.getBlock()).getRelative(b.getFacing())).getLocation();
         }
     }
 }
